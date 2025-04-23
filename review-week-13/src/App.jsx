2561961bd6f5f11427bbae5400147039ff905213
Jessevaneijk <jessejamesvaneijk@gmail.com>
2025-04-24 10:51:27 +1200

    github

--- review-week-13/src/App.jsx
+++ review-week-13/src/App.jsx
@@ -1,22 +1,23 @@
 import React, { useState } from 'react';
 import FruitList from './components/FruitList';
+import Message from './components/message';
 
 function App() {
   const [totalFruits, setTotalFruits] = useState(0);
   // This is the goal for the number of fruits to be eaten per day
-  const goal = 5;
+  const goal = 10;
 
   const handleTotalChange = (newTotalFruits) => {
     setTotalFruits(newTotalFruits);
   };
 
-  return (
-    <div style={{ textAlign: 'center', fontFamily: 'sans-serif' }}>
+return (
+  <div style={{ textAlign: 'center', fontFamily: 'sans-serif' }}>
       <h1>🍎 Healthy Eating Tracker</h1>
       <FruitList 
-        fruits={["Feijoa", "Apple",  "Avocado"]} 
+        fruits={["Feijoa", "Apple",  "Avocado", "Orange", "Banana"]} 
         onTotalChange={handleTotalChange} 
-      />
+        />
     </div>
   );
 }
